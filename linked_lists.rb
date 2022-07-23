523620219f625dc73f35848f469a510c154094d9
Red <crabmarket56@gmail.com>
2022-07-23 17:48:01 -0400

    Restart logic

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,40 +1,26 @@
 class Node
-  def value
-    nil
+  attr_accessor :value, :next_node
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
-
-  def next_node
-    nil
-  end
-
 end
 
 class LinkedList
   def append(value)
-    LinkedList.tail = old_tail
-    tail = Node.new
-    tail.value = value
-    old_tail.next_node = tail
+  
   end
 
   def prepend(value)
-    LinkedList.head = old_head
-    head = Node.new
-    head.value = value
-    head.next_node = old_head
+
   end
 
   def size
-    size = 1
-    node_tmp = head
-    until(node_tmp == nil)
-      node_tmp = node_tmp.next_node
-      size += 1
-    end
-    size 
+ 
   end
 
   def head
+    
 
   end
 
